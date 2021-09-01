06fd1c7a8f8de8ff0d6ac24fc205493c1677cd23
Ana María Ramos <anamaria.ramoshenriquez@gmail.com>
2021-08-31 23:01:11 -0400

    se agrego react con jsx

---
+++ 001IntroToReact/app-intro-to-react/src/components/PrimerReactJsx.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PrimerReactJsx = () => {
+
+    return (
+        <div>
+            <h1>Hola Dojo!</h1>
+            <h2>Thing I need to do:</h2>
+
+            <table>
+                <ul>
+                    <li>Learn to React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run Manhattan</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </table>
+        </div>
+    )
+
+}
+
+export default PrimerReactJsx
